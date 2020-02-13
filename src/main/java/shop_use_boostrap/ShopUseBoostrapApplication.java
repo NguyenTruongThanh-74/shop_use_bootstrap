bdb5d16455f26c4e12f3622b8bfe448f08ce771f
nguyentruongthanh-abc <nguyentruongthanhbk@gmail.com>
2020-02-13 22:20:03 +0700

    our commit

---
+++ src/main/java/shop_use_boostrap/ShopUseBoostrapApplication.java
@@ -0,0 +1,13 @@
+package shop_use_boostrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopUseBoostrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopUseBoostrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop_use_boostrap/ShopUseBoostrapApplicationTests.java
@@ -0,0 +1,13 @@
+package shop_use_boostrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopUseBoostrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
